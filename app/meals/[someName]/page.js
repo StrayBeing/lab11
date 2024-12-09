df773314f82cc584fcaa15cd25336ed01abd60b1
StrayBeing <maciej.sobierajski.zsti@gmail.com>
2024-12-09 19:12:54 +0100

    Test 2

--- app/meals/[someName]/page.js
+++ app/meals/[someName]/page.js
@@ -1,8 +1,17 @@
-export default function MealDetailPage({ params }) {
+export function generateStaticParams() {
+    const meals = ['burger', 'pizza', 'schnitzel'];
+    
+    return meals.map((meal) => ({
+      someName: meal,
+    }));
+  }
+  
+  export default function MealDetailPage({ params }) {
     return (
-        <div>
-            <h1>Meal: {params.someName}</h1>
-            <p>Current URL: /meals/{params.someName}</p>
-        </div>
+      <div>
+        <h1>Meal: {params.someName}</h1>
+        <p>Current URL: /meals/{params.someName}</p>
+      </div>
     );
-}
+  }
+  
